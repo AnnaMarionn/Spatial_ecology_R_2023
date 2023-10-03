bf1d09f8f90b2a1f6fbf9fb814b3dc50ef699662
AnnaMarionn <146854389+AnnaMarionn@users.noreply.github.com>
2023-10-03 17:47:23 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -12,3 +12,21 @@ final<- duccio*zima
 
 final^2
 # the arrow is to assign, equal is equal.
+# array with arguments
+sophi <- c(10,20,30,50,70) # microplastics
+paula <- c(100, 500, 600, 1000, 2000) # people
+plot(paula, sophi)
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+people <- paula
+microplastics <- sophie
+
+plot(people, microplastics)
+
+#in c++ it would be difficult
+# points characters, look it up online, "pch"
+plot (people, microplastics, pch=19)
+#cex= character exaggeration to increase the dimention of the point
+#col= change color, as CHARACTER (use "")
+plot (people, microplastics, pch=19, cex=2, col="blue")
+
+
