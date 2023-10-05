6f11274b5d879334da6f85f670a37b46e977b92e
AnnaMarionn <146854389+AnnaMarionn@users.noreply.github.com>
2023-10-05 17:56:33 +0200

    Create 02.populations.R

---
+++ 02.populations.R
@@ -0,0 +1,34 @@
+# code related to population ecology
+
+# a package is needed for point pattern analysis
+# adding package from cran??
+install.packages("spatstat")
+libraru(spatstat)
+
+#let's use the bei dataset
+# data description:
+#https://CRAN.R-project.org/package=spatstat
+
+bei
+
+#plotting the data
+plot(bei)
+#changing graphic- cex (0.5 or .5) and pch (symbol)
+
+plot(bei, cex=0.2, pch=19)
+
+#to see an image file (raster)
+bei.extra
+plot(bei.extra)
+
+#let's use only part of the bei dataset: elev
+bei.extra$elev
+
+#simplifying
+
+elevation <- bei.extra$elev
+plot(elevation)
+# selecting the first element
+
+elevation2 <- bei.extra[[1]]
+plot (elevation2)
