5375d39cdabd0e8dfea5a776ef3c554cdd94d922
AnnaMarionn <146854389+AnnaMarionn@users.noreply.github.com>
2023-11-14 17:40:26 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -29,4 +29,38 @@ plot(dif1, col=cl)
 #red= higher in jan and blue=higher value in march
 
 #data about ice melt and t change in greenland--> copernicus
+im.list()
+#"greenland.2000.tif"                                
+# "greenland.2005.tif"      
+#"greenland.2010.tif"                                
+#"greenland.2015.tif"  are temperature data from greenland of surface t, not air t
 
+g2000 <- im.import("greenland.2000.tif")
+plot(g2000, col=cl)
+
+#to import avery image:
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+par(mfrow=c(1,2))
+plot(g2000, col=cl)
+plot(g2015, col=cl)
+
+#stacking the data, in this case we only have one layer
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg)
+
+#difference between first and last element
+difg= g2000-g2015
+or
+difg <- stackg[[1]]-stackg[[4]]
+
+plot(difg, col=cl)
+
+#next lecture: import our own data (not imageRy)
+#element of stackg correspond to different images, we put them in rbg system and each will be represented by a color (1->red, 2->ble, 3->green)
+
+#to make an rgb plot using different years:
+im.plotRGB(stackg, r=1, g=2, b=3)
+#the colors we get show when the temperature was higher in that place (red=2000, green=2005 black/darkblue=2010)
