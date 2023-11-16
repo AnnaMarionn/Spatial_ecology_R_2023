f1d860f515cb335fccc3ae1c137e81032d60fc7c
AnnaMarionn <146854389+AnnaMarionn@users.noreply.github.com>
2023-11-16 17:25:20 +0100

    Update external.data.R

--- external.data.R
+++ external.data.R
@@ -13,3 +13,19 @@ naja <- rast("najafiraq_etm_2003140_lrg.jpg")
 
 #to plot: plotRGB, equal to im.plotRGB :
 plotRGB(naja, r=1, g=2, b=3)
+
+#now we download the other one (earth observatory, nasa) in the same folder
+#we don't need to set the working directory again
+
+najaaug <- rast("najafiraq_oli_2023219_lrg.jpg")
+plotRGB(najaaug, r=1, g=2, b=3)
+
+dev.off()
+par(mfrow=c(2,1))
+plotRGB(naja, r=1, g=2, b=3)
+plotRGB(najaaug, r=1, g=2, b=3)
+
+#multitemporal change detection
+najadif=naja[[1]] - najaaug[[1]]
+cl<-colorRampPalette(c("brown", "gray", "orange"))(100)
+plot(najadif, col=cl)
